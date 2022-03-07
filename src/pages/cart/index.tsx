1ff643548854c0eca25eaa75f4aabc08e54785da
Kovács Lajos <lajoskvcs@icloud.com>
2022-03-07 21:24:11 +0100

    #6/fix: Fixed indentation and import in cart page component

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,9 +1,12 @@
-import Layout from '../../components/Layout';
+import Layout from '@/components/Layout';
 
-const CartPage = () => (
-  <Layout>
-    <h1>Your Cart</h1>
-  </Layout>
-);
+const CartPage = () => {
+
+    return (
+        <Layout>
+            <h1>Your Cart</h1>
+        </Layout>
+    );
+}
 
 export default CartPage;
