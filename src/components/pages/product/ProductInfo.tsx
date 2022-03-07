f7849927a90dc5b230c305f7431f536d61e8da7f
Kovács Lajos <lajoskvcs@icloud.com>
2022-03-07 20:37:03 +0100

    #7/feat: Added add to cart button with quantity input on product info page

--- src/components/pages/product/ProductInfo.tsx
+++ src/components/pages/product/ProductInfo.tsx
@@ -1,6 +1,8 @@
 import useSWR from 'swr'
 import Image from 'next/image'
 import { Product } from '@/types'
+import { useState } from 'react'
+import { useAppContext } from '@/store'
 function fetcher (input: RequestInfo, init?: RequestInit): any {
 	return fetch(input, init).then(res => res.json())
 }
@@ -19,10 +21,17 @@ function useProductInfo(gtin: string, productData?: Product): [Product | undefin
 }
 
 function ProductInfo ({ productData, gtin }: ProductInfoProps) {
+	const appContext = useAppContext()
 	const [data, error] = useProductInfo(gtin, productData)
+	const [quantity, setQuantity] = useState(1)
+
 
 	if (error) return <div>failed to load</div>
 	if (!data) return <div>loading...</div>
+
+	const addToCart = () => {
+		appContext.addItemToTheCart(data, quantity)
+	}
 
 	return (
 		<div>
@@ -36,6 +45,15 @@ function ProductInfo ({ productData, gtin }: ProductInfoProps) {
 			<div>{data.categoryName}</div>
 			<div>{data.brandName}</div>
 			<div>{data.recommendedRetailPrice} {data.recommendedRetailPriceCurrency}</div>
+			<div className="flex">
+				<input type="number" placeholder="Type here"
+					   className="input input-bordered input-primary w-full max-w-xs"
+					   value={quantity}
+					   min="1"
+					   onChange={(e) => {setQuantity(parseInt(e.target.value))}}
+				/>
+				<button className="btn btn-secondary" onClick={() => addToCart()}>Add to cart</button>
+			</div>
 		</div>
 	)
 }
