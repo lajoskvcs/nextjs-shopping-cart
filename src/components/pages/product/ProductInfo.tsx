30f082939e2cf6a15329d3c33d1414ea68cbaadf
Kovács Lajos <lajoskvcs@icloud.com>
2022-03-08 10:09:01 +0100

    #4/feat: Redesigned product info page

--- src/components/pages/product/ProductInfo.tsx
+++ src/components/pages/product/ProductInfo.tsx
@@ -35,25 +35,46 @@ function ProductInfo ({ productData, gtin }: ProductInfoProps) {
 
 	return (
 		<div>
-			<Image
-				src={data.imageUrl}
-				alt={data.name}
-				width={500}
-				height={500}
-			/>
-			<div>{data.name}</div>
-			<div>{data.categoryName}</div>
-			<div>{data.brandName}</div>
-			<div>{data.recommendedRetailPrice} {data.recommendedRetailPriceCurrency}</div>
-			<div className="form-control">
-				<div className="input-group">
-					<input type="number" placeholder="Type here"
-						   className="input input-bordered input-primary w-full max-w-xs"
-						   value={quantity}
-						   min="1"
-						   onChange={(e) => {setQuantity(parseInt(e.target.value))}}
+			<div className="flex justify-between mt-5 items-center">
+				<div>
+					<table className="table">
+						<tr>
+							<th>Name</th>
+							<td>{data.name}</td>
+						</tr>
+						<tr>
+							<th>Brand</th>
+							<td>{data.brandName}</td>
+						</tr>
+						<tr>
+							<th>Category</th>
+							<td>{data.categoryName}</td>
+						</tr>
+						<tr>
+							<th>Price</th>
+							<td>{data.recommendedRetailPrice} {data.recommendedRetailPriceCurrency}</td>
+						</tr>
+					</table>
+					<div className="form-control">
+						<div className="input-group">
+							<input type="number" placeholder="Type here"
+								   className="input input-bordered input-primary w-full max-w-xs"
+								   value={quantity}
+								   min="1"
+								   onChange={(e) => {setQuantity(parseInt(e.target.value))}}
+							/>
+							<button className="btn btn-secondary" onClick={() => addToCart()}>Add to cart</button>
+						</div>
+					</div>
+				</div>
+				<div className="shadow-lg rounded-2xl">
+					<Image
+						src={data.imageUrl}
+						alt={data.name}
+						width={500}
+						height={500}
+						className="rounded-2xl"
 					/>
-					<button className="btn btn-secondary" onClick={() => addToCart()}>Add to cart</button>
 				</div>
 			</div>
 		</div>
